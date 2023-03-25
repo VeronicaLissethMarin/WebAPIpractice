2759af903d31245f137064e86628adca40816902
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-03-24 23:55:36 -0600

    Finalizando CRUD

--- WebAPIpractice/Controllers/carrerasController.cs
+++ WebAPIpractice/Controllers/carrerasController.cs
@@ -14,6 +14,75 @@ namespace WebAPIpractice.Controllers
             _equiposContexto = equiposContexto;
         }
 
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAllCarreras()
+        {
+            var listadoCarreras = (from e in _equiposContexto.carreras
+                                   join B in _equiposContexto.facultades on e.facultad_id equals B.facultad_id
+                                   select new
+                                   {
+                                       e,
+                                       B.nombre_facultad
+                                   }).ToList();
+
+            if (listadoCarreras.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoCarreras);
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            var carreras = (from e in _equiposContexto.carreras
+                            join F in _equiposContexto.facultades
+                            on e.facultad_id equals F.facultad_id
+                            where e.carrera_id == id
+                            select new
+                            {
+                                e.carrera_id,
+                                e.nombre_carrera,
+                                F.facultad_id,
+                                F.nombre_facultad
+                            }).FirstOrDefault();
+
+            if (carreras == null)
+            {
+                return NotFound();
+            }
+            return Ok(carreras);
+        }
+
+        [HttpGet]
+        [Route("Find/{filtro}")]
+
+        public IActionResult FindByName(string filtro)
+        {
+            var Listadocarreras = (from e in _equiposContexto.carreras
+                                   join F in _equiposContexto.facultades
+                                      on e.facultad_id equals F.facultad_id
+                                   where e.nombre_carrera.Contains(filtro)
+                                   select new
+                                   {
+                                       e.carrera_id,
+                                       e.nombre_carrera,
+                                       F.facultad_id,
+                                       F.nombre_facultad
+                                   }).FirstOrDefault();
+
+            if (Listadocarreras == null)
+            {
+                return NotFound();
+            }
+            return Ok(Listadocarreras);
+        }
+
+
         [HttpPost]
         [Route("Add")]
         public IActionResult GuardarCarrera([FromBody] estados_equipo carrera)
@@ -74,5 +143,9 @@ namespace WebAPIpractice.Controllers
 
             return Ok(carrera);
         }
+
+
+        
+
     }
 }
